4c8a77d4c461044f466bfd2934f19aaab736af6f
Jung-kr <jungwy98@gmail.com>
2023-08-06 15:21:11 +0900

    feat : 게시글 상세 구현

--- src/main/java/com/jung/board/question/QuestionController.java
+++ src/main/java/com/jung/board/question/QuestionController.java
@@ -2,11 +2,13 @@ package com.jung.board.question;
 
 import java.util.List;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -19,5 +21,12 @@ public class QuestionController {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
